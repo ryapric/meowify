44c6c43b7a7c0d11dc78c5967d1a43dee9f1560f
Ryan J. Price <ryapric@gmail.com>
2020-04-29 16:04:20 -0500

    Add meowcode

---
+++ meowify.py
@@ -0,0 +1,9 @@
+import sys
+import string
+
+letters = list(string.ascii_letters)
+numbers = list(range(10))
+allowed = letters + numbers
+
+txt = sys.argv[1]
+print(''.join([f':meow_{t}:' if t in allowed else t for t in txt]))
